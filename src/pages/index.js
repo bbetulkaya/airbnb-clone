8438a8304f7d9b733af1f5c3e9a760dd1afc95ca
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2024-01-28 15:03:02 +0300

    feat: create a header and config for image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
+import Header from "@/components/Header";
+import Head from "next/head";
+
 export default function Home() {
-  return <div>this is home page</div>;
+  return (
+    <div>
+      <Head>
+        <title>Airbnb Clone by Betul</title>
+      </Head>
+
+      <Header />
+      {/* Banner */}
+    </div>
+  );
 }
