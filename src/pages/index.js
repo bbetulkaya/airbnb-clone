ca0524e842fef24082d55b78215a84401f63ba93
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2024-02-01 14:47:07 +0300

    feat: create cards and data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
 import Banner from "@/components/Banner";
 import Header from "@/components/Header";
 import Head from "next/head";
+import exploreData from "../../public/data/exploreData.json";
+import stayCategories from "../../public/data/stayCategories.json";
+
+import SmallCard from "@/components/SmallCard";
+import MediumCard from "@/components/MediumCard";
+import LargeCard from "@/components/LargeCard";
 
 export default function Home() {
   return (
@@ -11,6 +17,37 @@ export default function Home() {
 
       <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData.map((item, index) => (
+              <SmallCard
+                key={index}
+                img={item.img}
+                distance={item.distance}
+                location={item.location}
+              />
+            ))}
+          </div>
+        </section>
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <div className="flex space-x-3 overflow-x-auto overflow-y-hidden scrollbar-hide p-3 -ml-3">
+            {stayCategories.map((item, index) => (
+              <MediumCard key={index} img={item.img} title={item.title} />
+            ))}
+          </div>
+        </section>
+
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Wishlists curated by Airbnb."
+          buttonText="Get Inspired"
+        />
+      </main>
     </div>
   );
 }
