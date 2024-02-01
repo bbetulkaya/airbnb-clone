20c661a57976b20dd6d083d08a83762e38c4a56c
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2024-02-01 15:23:48 +0300

    refactor: change all img sources

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ module.exports = {
           hostname: 'links.papareact.com',
           pathname: '**',
         },
+        {
+          protocol: 'https',
+          hostname: 'a0.muscache.com',
+          pathname: '**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'upload.wikimedia.org',
+          pathname: '**',
+        }
       ],
   },
 };
